9bff4711d3e7c523a1f45f681571d733e313701d
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-31 13:16:27 +0530

    uc1

---
+++ AddressBook/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Customer
+    {
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string state { get; set; }
+        public int pin { get; set; }
+        public long num { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the address book program");
+            List<Customer> l = new List<Customer>();
+            Customer customer1 = new Customer { name = "jayaprakash",lastname ="Mudigonda",Address="warangal",City="hanamkonda",state="Telangana", pin = 506342, num = 7337545935,Email="jayaprakashjp187@gmail.com" };
+  
+            l.Add(customer1);
+            foreach (Customer details in l)
+            {
+                Console.WriteLine("name=" + details.name);
+
+                Console.WriteLine("lastname=" + details.lastname);
+                Console.WriteLine("address=" + details.Address);
+                Console.WriteLine("city=" + details.City);
+                Console.WriteLine("state=" + details.state);
+                Console.WriteLine("pin" + details.pin);
+                Console.WriteLine("phonenumber=" + details.num);
+                Console.WriteLine("email=" + details.Email);
+            }
+        }
+    }
+}
