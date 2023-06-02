128597f5e11cd8342d56c4b83246ce6d46b965f9
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-02 12:29:44 +0530

    deleting one contact in address variable

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -10,7 +10,7 @@ namespace AddressBook
         {
 
             Console.WriteLine("welcome to the address book program");
-            Console.WriteLine("1.create a contact in address book\n2.Add Contact in address book\n3.Edit contacts in a address book");
+            Console.WriteLine("1.create a contact in address book\n2.Add Contact in address book\n3.Edit contacts in a address book\n4.Delete contacts in address book");
             Console.WriteLine("choose what proogram u want");
             int choose = Convert.ToInt32(Console.ReadLine());
             switch (choose)
@@ -76,6 +76,32 @@ namespace AddressBook
                     }
 
                     break;
+                  case 4:
+                    List<Customer> a = new List<Customer>();
+                    Customer k = new Customer { name = "jayaprakash", lastname = "Mudigonda", Address = "warangal", City = "hanamkonda", state = "Telangana", pin = 506342, num = 7337545935, Email = "jayaprakashjp187@gmail.com" };
+                    Customer s = new Customer { name = "pallavi", lastname = "ediga", Address = "shivmogga", City = "bangalore", state = "karnataka", pin = 560001, num = 6361419518, Email = "pallavig1997@gmail.com" };
+
+                    a.Add(k);
+                    a.Remove(s); //deleting s data
+                    Console.WriteLine("after deleting s variable");
+                    foreach (Customer details in a)
+                    {
+                        Console.WriteLine("name=" + details.name);
+
+                        Console.WriteLine("lastname=" + details.lastname);
+                        Console.WriteLine("address=" + details.Address);
+                        Console.WriteLine("city=" + details.City);
+                        Console.WriteLine("state=" + details.state);
+                        Console.WriteLine("pin" + details.pin);
+                        Console.WriteLine("phonenumber=" + details.num);
+                        Console.WriteLine("email=" + details.Email);
+                    }
+                    break; 
+                
+                    
+
+
+                        
 
 
             } 
